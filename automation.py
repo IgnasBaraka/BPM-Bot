644467bbe63302049d615cffbe35878c40f6103d
IgnasBaraka <67372272+IgnasBaraka@users.noreply.github.com>
2020-07-05 14:58:44 +0300

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome()
+driver.get("https://www.marktplaats.nl/l/auto-s/f/diesel+sedan/474+483/#PriceCentsTo:500000|constructionYearFrom:2007|mileageFrom:200000")
+elem = driver.find_element_by_xpath('//*[@id="content"]/div[2]/ul/li[2]/a')
+elem.click()
+element = driver.find_element_by_xpath('//*[@id="car-attributes"]/div[1]/div[7]/span[2]')
+
+actions = ActionChains(driver)
+
+actions.move_to_element(element).perform()
+
+kenteken = driver.find_element_by_xpath('//*[@id="car-attributes"]/div[1]/div[5]/span[2]')
+print (kenteken.text)
+text = kenteken.text
 
 
-with webdriver.Chrome() as driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get("https://www.marktplaats.nl/a/auto-s/audi/m1557624092-audi-a6-2-0-tdi-a-business-edition-3-x-s-line-verkocht.html?c=df2f21f683612b45d62c413c0ca719df&previousPage=lr")
-    Kenteken = driver.find_elements_by_xpath('//*[@id="car-attributes"]/div[1]/div[5]/span[2]')
-    print(Kenteken)
+
+
+driver.get('https://www.finnik.nl/?referrer')
+
+element = driver.find_element_by_xpath('/html/body/div[2]/form/button')
+element.click()
+search = driver.find_element_by_xpath('/html/body/section[1]/div/div[1]/article[2]/div/form/div/input')
+search.click()
+
+search.send_keys(text)
